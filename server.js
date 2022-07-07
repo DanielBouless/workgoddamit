210c3637ff086334b47b379cd4be567c3158651d
Daniel Bouless <dbouless76@gmail.com>
2022-07-06 20:56:53 -0400

    added form

--- server.js
+++ server.js
@@ -2,24 +2,37 @@
 const express = require('express')
 const breads = express.Router()
 
+
 // CONFIGURATION
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
+
+// MIDDLEWARE
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+
+// CONTROLLERS
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
+
 
 // ROUTES
 app.get('/', (req, res) => {
   res.send('Welcome to an Awesome App about Breads')
 })
 
-// Breads
-const breadsController = require('./controllers/breads_controller.js')
-app.use('/breads', breadsController)
+app.get('*', (req,res)=>{
+  res.send('404')
+})
 
-// MIDDLEWARE
+
+
+
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+
 
 // LISTEN
 app.listen(PORT, () => {
